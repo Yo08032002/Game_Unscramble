6bbdbda852a6d8b01ee2905abb6952d9bcd3acd7
Yo08032002 <8955855533mso@gmail.com>
2023-12-15 19:02:57 +0530

    project

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and 
- * limitations under the License.
- */
 
 package com.example.android.unscramble.ui.game
 
@@ -134,11 +119,11 @@ class GameFragment : Fragment() {
     */
     private fun setErrorTextField(error: Boolean) {
         if (error) {
-            binding.textField.isErrorEnabled = true
+            binding.textField.isErrorEnabled = true   //red border line
             binding.textField.error = getString(R.string.try_again)
         } else {
             binding.textField.isErrorEnabled = false
-            binding.textInputEditText.text = null
+            binding.textInputEditText.text = null  //empty the input
         }
     }
 }
